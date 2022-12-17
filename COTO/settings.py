18364f93f4aa7792975df6fb2bd8bff57bd7185f
CarlosCruz1503 <carlosjose445566@gmail.com>
2022-12-17 09:47:04 -0500

    database fixed

--- COTO/settings.py
+++ COTO/settings.py
@@ -87,12 +87,14 @@ WSGI_APPLICATION = "COTO.wsgi.application"
 #    "NAME": BASE_DIR / "db.sqlite3",
 # }
 # }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default': dj_database_url.config(        
+    # Feel free to alter this value to suit your needs.        
+    default='postgresql://postgres:postgres@localhost:5432/mysite',        
+    conn_max_age=600    
+)}
 
 
 # Password validation
