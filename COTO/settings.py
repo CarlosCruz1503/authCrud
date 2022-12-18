e441da4b144a5f370d908eae620ff70247be6ada
CarlosCruz1503 <carlosjose445566@gmail.com>
2022-12-18 15:50:10 -0500

    change database to elephantsql

--- COTO/settings.py
+++ COTO/settings.py
@@ -82,19 +82,15 @@ WSGI_APPLICATION = "COTO.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-       "ENGINE": "django.db.backends.sqlite3",
-      "NAME": BASE_DIR / "db.sqlite3",
-  }
-}
 
-#DATABASES = {
-#  'default': dj_database_url.config(
-#        # Feel free to alter this value to suit your needs.
-#       default='postgresql://postgres:postgres@localhost:5432/mysite',
-#       conn_max_age=600
-#)}
+
+DATABASES = {
+  'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+       default='postgres://iccwsutf:asRqEdtcZu894yCfT2CExsQyLUPtDKEd@babar.db.elephantsql.com/iccwsutf',
+       conn_max_age=600
+)}
+
 
 
 # Password validation
